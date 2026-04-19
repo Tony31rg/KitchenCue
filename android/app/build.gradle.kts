d25fa07e2170c5c9f9d201b00d5fb3e33e6ae865
Nang Shwe Sin <6731503072@lamduan.mfu.ac.th>
2026-04-19 20:29:39 +0700

    latest version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.kitchen_cue"
+    namespace = "com.kitchencue.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,8 +31,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kitchen_cue"
+        applicationId = "com.kitchencue.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/kitchen_cue/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kitchen_cue/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kitchen_cue
+package com.kitchencue.app
 
 import io.flutter.embedding.android.FlutterActivity
 
